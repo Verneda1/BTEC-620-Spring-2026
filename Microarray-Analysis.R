9945ebcb884248e99dda75b07c276db9e4d5ed1b
Verneda Ritter <veritter@claflin.edu>
2026-01-27 13:45:43 -0500

    Add files via upload

---
+++ Microarray-Analysis.R
@@ -0,0 +1,12 @@
+#Name: Verneda Ritter; Date:01/27/2026; Purpose: Microarray Analysis 
+
+#Install Affymetrix library/Package 
+
+if (!require("BiocManager", quietly = TRUE))
++     install.packages("BiocManager")
+
+BiocManager::install("affy")
+
+library(affy)
+
+setwd("/Users/verneda/Desktop")
