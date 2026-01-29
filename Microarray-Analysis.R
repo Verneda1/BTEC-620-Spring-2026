64031d2e184f88300ab1a5c549987edfa94c0563
Verneda Ritter <veritter@claflin.edu>
2026-01-29 13:35:26 -0500

    Add files via upload

--- Microarray-Analysis.R
+++ Microarray-Analysis.R
@@ -10,3 +10,11 @@ BiocManager::install("affy")
 library(affy)
 
 setwd("/Users/verneda/Desktop")
+
+#Read the 4 .CEL files in R
+data <- ReadAffy()
+
+#Generate a box to visable dataset 
+boxplot(data)
+
+# Normalize the dataset to remove ant outliers and stable the median for all .CEL files 
